dc40944830582dd7bf6da5710fd90ac090c43036
NishantGupta827 <nishant.gupta@contentstack.com>
2025-05-18 15:59:32 +0530

    feat:added node and edge state

--- src/components/BasicFlow.tsx
+++ src/components/BasicFlow.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   ReactFlow,
   Background,
   Controls,
   MiniMap,
   ReactFlowProvider,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  type Connection,
+  type Edge,
 } from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
-
 
 import { type CanvasConfig, type FlowJson } from '../type';
 import { ParseBackground } from './BackGround';
@@ -18,24 +22,34 @@ export interface BasicFlowProps {
 }
 
 const BasicFlow: React.FC<BasicFlowProps> = ({ json }) => {
-  const { nodes, edges, canvas } = json;
+  const { canvas } = json;
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(json.nodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(json.edges);
+
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
 
   return (
-    <>
-      <ReactFlowProvider>
-        <div style={{ width: '100%', height: '600px' }}>
-          <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            fitView
-          >
-            <Background {...ParseBackground(canvas as CanvasConfig)}/>
-            {canvas?.controls && <Controls />}
-            {canvas?.minimap && <MiniMap />}
-          </ReactFlow>
-        </div>
-      </ReactFlowProvider>
-    </>
+    <ReactFlowProvider>
+      <div style={{ width: '100%', height: '600px' }}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          nodesDraggable={true}
+          fitView
+        >
+          <Background {...ParseBackground(canvas as CanvasConfig)} />
+          {canvas?.controls && <Controls />}
+          {canvas?.minimap && <MiniMap />}
+        </ReactFlow>
+      </div>
+    </ReactFlowProvider>
   );
 };
 
