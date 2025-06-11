42e24e724c412dbaf5b5fa379f4c80865b6cec52
killerrekt <amankhanter14@gmail.com>
2025-06-11 11:04:18 +0530

    Fix: Edge button fixed

--- src/utils/layoutUtil.ts
+++ src/utils/layoutUtil.ts
@@ -34,8 +34,9 @@ export function getLayoutedElements(
 
   dagreGraph.setGraph({
     rankdir: direction,
-    nodesep: !isHorizontal ? 3 * max_height : 100,
-    ranksep: !isHorizontal ? 100 : max_width,
+    ranksep: max_width * 0.5,
+    //nodesep: !isHorizontal ? 3 * max_height : 100,
+    //ranksep: !isHorizontal ? 100 : max_width,
   });
 
   edges.forEach((edge) => {
