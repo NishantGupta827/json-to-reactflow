5067c3a3fff2a56f0ee8614b8745a23223badee1
NishantGupta827 <nishant.gupta@contentstack.com>
2025-07-09 15:02:23 +0530

    nuke repo(half)

--- src/types/flowJson.ts
+++ src/types/flowJson.ts
@@ -1,15 +1,5 @@
 import { Edge, Node } from "@xyflow/react";
 
-export interface BackgroundConfig {
-  bgcolor?: string;
-  color?: string;
-  variant?: "dots" | "lines" | "cross";
-  size?: number;
-  gap?: number;
-  lineWidth?: number;
-  gridSize?: number;
-}
-
 type PartialNodeInput = Partial<Pick<Node, "type" | "position">> &
   Pick<Node, "id"> & { data?: any };
 
